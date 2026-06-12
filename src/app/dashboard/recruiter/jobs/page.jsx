6cb4c1ec5834b9dcadee45bdd9848a64e7b2e7de
Farhan Sadiq <farhansadiq2021@gmail.com>
2026-06-12 16:11:22 +0600

    Added feature to add a new job in recruiter dashboard

---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecruiterJobs = () => {
+    return (
+        <div>
+            <h2 className='text-white'>All jobs page of recruiter Dashboard</h2>
+        </div>
+    );
+};
+
+export default RecruiterJobs;
